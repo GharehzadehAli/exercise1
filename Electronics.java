2491e550fedcb1ad4bc9be8c47629f794717d509
GharehzadehAli <gharehzadehali@gmail.com>
2019-03-29 23:09:31 +0430

    class Electronics created

--- Electronics.java
+++ Electronics.java
@@ -1,4 +1,19 @@
 package com.company;
 
-public class Electronics {
+public abstract class Electronics {
+    private double price;
+    private String name;
+
+    Electronics(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
 }
