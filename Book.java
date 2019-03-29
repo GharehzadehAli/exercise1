8e8343e9787d30970d1d190504fa13cce14617ec
GharehzadehAli <gharehzadehali@gmail.com>
2019-03-29 23:56:55 +0430

    subclass Book created

---
+++ Book.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Book extends Notes{
+    private String Wtiter;
+    Book(String name,double price,String wtiter){
+        super(name,price);
+        this.Wtiter=wtiter;
+    }
+
+}
